72ea14a5e50adc0cbf2408fbb095d45f07a46b32
Pratik Shinde <44629924+PratikShinde-2012@users.noreply.github.com>
2023-01-12 15:14:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ function getIp() {
     $.getJSON(API_URLs.GET_IP_API_URL, function(data) {
         $("#ip-address").html(data.ip);
         const userIp = data.ip
-        console.log(userIp)
-
     })
 }
 getIp();
